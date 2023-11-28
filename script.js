17b25205d9fc27571e3d7a1887b8a4ef2960bca7
Milltiades <gigakakulia2017@gmail.com>
2023-11-28 13:42:32 +0400

    workout with number methods: toFixed(), toPrecision() and toExponental()

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // let num = 3;
-let newNum = 3.16;
+// let newNum = 3.16;
 
 // console.log(typeof num);
 // console.log(typeof newNum);
 
-let mill = 1e6;
+// let mill = 1e6;
 // 1 * (10**6) = 1000000
-console.log(mill);
+// console.log(mill);
 
-let smallNum = 1e-2;
+// let smallNum = 1e-2;
 
-console.log(smallNum);
+// console.log(smallNum);
 
 // let score = new Number(100);
-console.log(newNum.toFixed());
+// console.log(newNum.toFixed());
 
-let num = 1e5;
-console.log("num: ", typeof num);
+// let num = 1e5;
+// console.log("num: ", typeof num);
+
+const PI = 3.141596254;
+
+let num = PI.toFixed(3); // დაამრგვალო სიზუსტით
+// num = PI.toPrecision(5); // ზუსტად გამოაჩინო
+
+let mill = 1000000;
+// num = mill.toExponential(); // შეკვეცილათ აჩვენო
+
+let num2 = PI.toPrecision(3);
+console.log(num2);
+let lightSpeed = 186000;
+
+console.log(lightSpeed.toExponential());

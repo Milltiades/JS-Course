fac7ca4ce8c854bbaa8c5c7f7f0b9155a503f52d
Milltiades <gigakakulia2017@gmail.com>
2023-11-28 13:28:52 +0400

    introduce with number

---
+++ script.js
@@ -0,0 +1,19 @@
+// let num = 3;
+let newNum = 3.16;
+
+// console.log(typeof num);
+// console.log(typeof newNum);
+
+let mill = 1e6;
+// 1 * (10**6) = 1000000
+console.log(mill);
+
+let smallNum = 1e-2;
+
+console.log(smallNum);
+
+// let score = new Number(100);
+console.log(newNum.toFixed());
+
+let num = 1e5;
+console.log("num: ", typeof num);

efed09d3133b953d25b140f3547a04336804b4df
Milltiades <gigakakulia2017@gmail.com>
2023-12-16 21:22:37 +0400

    work with oop principes, abstraction, encapsulation, inheritance and polymorphism

--- ListBinding.js
+++ ListBinding.js
@@ -104,26 +104,135 @@
 // const myPizza = pizzaFactory("small");
 // myPizza.bake();
 
-class Pizza {
-  crust = "original";
-  #sauce = "traditional";
-  #size;
-  constructor(pizzaSize) {
-    this.#size = pizzaSize;
-  }
-  getCrust() {
-    return this.crust;
-  }
-  setCrust(pizzaCrust) {
-    this.crust = pizzaCrust;
-  }
-  hereYouGo() {
-    console.log(
-      `Here's your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza.`
-    );
-  }
-}
-const myPizza = new Pizza("large");
-myPizza.hereYouGo();
+// class Pizza {
+//   crust = "original";
+//   #sauce = "traditional";
+//   #size;
+//   constructor(pizzaSize) {
+//     this.#size = pizzaSize;
+//   }
+//   getCrust() {
+//     return this.crust;
+//   }
+//   setCrust(pizzaCrust) {
+//     this.crust = pizzaCrust;
+//   }
+//   hereYouGo() {
+//     console.log(
+//       `Here's your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza.`
+//     );
+//   }
+// }
+// const myPizza = new Pizza("large");
+// myPizza.hereYouGo();
 
-console.log(myPizza.crust);
+// console.log(myPizza.crust);
+
+// //Abstraction
+// class Auth {
+//   constructor() {}
+
+//   #validate() {
+//     // Private method
+//   }
+
+//   #sendOnboardingEmail() {
+//     // Private method
+//   }
+
+//   // Public method that calls private methods
+//   signUp() {
+//     this.#validate();
+//     // Execute signUp functionality
+//     this.#sendOnboardingEmail();
+//   }
+// }
+
+// const auth = new Auth();
+
+// // Calls the public method, which, in turn, calls private methods
+// auth.signUp();
+
+// Encapsulation
+
+// function createCounter() {
+//   let count = 0; // This variable is encapsulated within the closure.
+
+//   function increment() {
+//     count++; // The inner function can access the 'count' variable.
+//     console.log(count);
+//   }
+
+//   return increment; // Return the inner function (closure).
+// }
+
+// const counter = createCounter();
+
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+// inheritance
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   sayHello() {
+//     console.log(
+//       `Hello, my name is ${this.name}, and I am ${this.age} years old.`
+//     );
+//   }
+// }
+
+// // Create a derived class Student that inherits from Person
+// class Student extends Person {
+//   constructor(name, age, studentId) {
+//     super(name, age); // Call the parent class constructor using super
+//     this.studentId = studentId;
+//   }
+
+//   study(subject) {
+//     console.log(
+//       `${this.name} with student ID ${this.studentId} is studying ${subject}.`
+//     );
+//   }
+// }
+
+// // Create instances of Person and Student
+// const person1 = new Person("Alice", 30);
+
+// const student1 = new Student("Bob", 25, "12345");
+
+// // Use the inherited methods
+// person1.sayHello();
+
+// student1.sayHello();
+
+// student1.study("Math");
+
+// polymorphism
+
+// class Transport {
+//   constructor() {}
+//   drive() {
+//     console.log("Driving...");
+//   }
+//   fly() {
+//     console.log("Flying...");
+//   }
+// }
+// // Create a derived class Car that inherits from Transport
+// class Car extends Transport {
+//   constructor() {
+//     super();
+//   }
+//   // Overrides it's base implementation
+//   fly() {
+//     console.log("I am a car, I cannot fly");
+//   }
+// }
+// const audi = new Car();
+// audi.fly();

14812ff1824892802315366fc3763aa11bb1dac2
Milltiades <gigakakulia2017@gmail.com>
2023-12-17 20:07:25 +0400

    working with getter and setter

--- gettersandsetters.js
+++ gettersandsetters.js
@@ -24,18 +24,44 @@
 // person.fullName = "Gedeon Kakulia";
 // console.log(person);
 
-const person = {
-  name: "giga",
-  surname: "kakulia",
-  get fullname() {
-    return `${this.name} ${this.surname}`;
-  },
-  set fullName(value) {
-    const parts = value.split(" ");
-    this.name = parts[0];
-    this.surname = parts[1];
-  },
-};
-person.fullName = "lasha kakhidze";
+// const person = {
+//   name: "giga",
+//   surname: "kakulia",
+//   get fullname() {
+//     return `${this.name} ${this.surname}`;
+//   },
+//   set fullName(value) {
+//     const parts = value.split(" ");
+//     this.name = parts[0];
+//     this.surname = parts[1];
+//   },
+// };
+// person.fullName = "lasha kakhidze";
 
-console.log(person.fullname);
+// console.log(person.fullname);
+
+// class Person {
+//   constructor(first, last) {
+//     this.first = first;
+//     this.last = last;
+//     this.count = 0;
+//   }
+//   get fullName() {
+//     this.count += 1;
+//     return `${this.first} ${this.last}`;
+//   }
+//   set fullName(value) {
+//     if (!value) {
+//       throw "BOO BANE CANNOT BE BLANK";
+//     }
+//     const [first, last] = value.split(" ");
+
+//     this.first = first;
+//     this.last = last;
+//   }
+// }
+
+// const actor = new Person("Brendan", "Fraser");
+// // actor.first = "gio";
+// actor.fullName = "Giga Kakulia";
+// console.log(actor.fullName);

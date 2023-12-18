8a093b09928c6add45b726c284a67abfe2ea347d
Milltiades <gigakakulia2017@gmail.com>
2023-12-18 18:12:19 +0400

    work with nested funciton ans closures

--- functions.js
+++ functions.js
@@ -244,13 +244,24 @@
 // }
 // foo(3);
 
-function addSquares(a, b) {
-  function square(x) {
-    return x * x;
+// function addSquares(a, b) {
+//   function square(x) {
+//     return x * x;
+//   }
+//   return square(a) + square(b);
+// }
+
+// console.log(addSquares(2, 3)); //13
+// console.log(addSquares(3, 4)); //25
+// console.log(addSquares(4, 5)); // 41
+
+function outside(x) {
+  function inside(y) {
+    return x + y;
   }
-  return square(a) + square(b);
+  return inside;
 }
 
-console.log(addSquares(2, 3)); //13
-console.log(addSquares(3, 4)); //25
-console.log(addSquares(4, 5)); // 41
+const fnInside = outside(3); // Think of it like: give me a function that adds 3 to whatever you give it
+console.log(fnInside(5)); // 8
+console.log(outside(3)(5)); // 8

264a9cad9b87001f0624e0b4f51ef885b2bd30c0
Milltiades <gigakakulia2017@gmail.com>
2023-12-18 18:15:05 +0400

    Multiply-nested functions

--- functions.js
+++ functions.js
@@ -255,13 +255,24 @@
 // console.log(addSquares(3, 4)); //25
 // console.log(addSquares(4, 5)); // 41
 
-function outside(x) {
-  function inside(y) {
-    return x + y;
-  }
-  return inside;
-}
+// function outside(x) {
+//   function inside(y) {
+//     return x + y;
+//   }
+//   return inside;
+// }
 
-const fnInside = outside(3); // Think of it like: give me a function that adds 3 to whatever you give it
-console.log(fnInside(5)); // 8
-console.log(outside(3)(5)); // 8
+// const fnInside = outside(3); // Think of it like: give me a function that adds 3 to whatever you give it
+// console.log(fnInside(5)); // 8
+// console.log(outside(3)(5)); // 8
+
+function A(x) {
+  function B(y) {
+    function C(z) {
+      console.log(x + y + z);
+    }
+    C(3);
+  }
+  B(2);
+}
+A(1); // Logs 6 (which is 1 + 2 + 3)

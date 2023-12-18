f79a96c31c18bc9f05c109abddb5d89cca95e739
Milltiades <gigakakulia2017@gmail.com>
2023-12-18 17:43:20 +0400

    calling functions

--- functions.js
+++ functions.js
@@ -107,13 +107,63 @@
 // myFunc(myCar);
 // console.log(myCar.make); // Toyto
 
-function myFunc(theArr) {
-  theArr[0] = 30;
+// function myFunc(theArr) {
+//   theArr[0] = 30;
+// }
+
+// const arr = [45];
+
+// console.log(arr[0]);
+
+// myFunc(arr);
+// console.log(arr[0]); // 30
+
+// const square = function (number) {
+//   return number * number;
+// };
+
+// console.log(square(4)); // 16
+
+// const factorial = function fac(n) {
+//   return n < 2 ? 1 : n * fac(n - 1);
+// };
+// console.log(factorial(5)); // 120
+// console.log(factorial(4)); // 24
+// console.log(factorial(3)); // 6
+
+// function map(f, a) {
+//   const result = new Array(a.length);
+//   for (let i = 0; i < a.length; i++) {
+//     result[i] = f(a[i]);
+//   }
+//   return result;
+// }
+// const cube = function (x) {
+//   return x * x * x;
+// };
+// const numbers = [0, 1, 2, 5, 10];
+// console.log(map(cube, numbers)); // [0, 1, 8, 125, 1000]
+
+// let myFunc;
+// let num = 0;
+// if (num === 0) {
+//   myFunc = function (theObject) {
+//     theObject.make = "Toyota";
+//   };
+// }
+
+// console.log(myFunc());
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 
-const arr = [45];
-
-console.log(arr[0]);
-
-myFunc(arr);
-console.log(arr[0]); // 30
+console.log(factorial(5)); // 120
+console.log(factorial(7)); // 5040
+console.log(factorial(3)); // 6
+console.log(factorial(1)); // 1
+console.log(factorial(0)); // 1

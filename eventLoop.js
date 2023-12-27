c8387be9df870ce29bfd464d92e74a66579e3302
Milltiades <gigakakulia2017@gmail.com>
2023-12-27 16:12:39 +0400

    Promises and Async Await

--- eventLoop.js
+++ eventLoop.js
@@ -14,14 +14,14 @@
 
 // console.log("end");
 
-console.log("start");
+// console.log("start");
 
-setTimeout(function cbT() {
-  console.log("CB SetTimeout");
-}, 5000);
+// setTimeout(function cbT() {
+//   console.log("CB SetTimeout");
+// }, 5000);
 
-fetch("https://api.netflix.com").then(function cbF() {
-  console.log("CB Netflix");
-});
+// fetch("https://api.netflix.com").then(function cbF() {
+//   console.log("CB Netflix");
+// });
 
-console.log("end");
+// console.log("end");

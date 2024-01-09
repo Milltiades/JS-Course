f3f260ed32e71734eed33dc81a5449f53f42c47b
Milltiades <gigakakulia2017@gmail.com>
2024-01-09 20:17:50 +0400

    generator composition

--- advanced.js
+++ advanced.js
@@ -131,25 +131,154 @@
 // console.log(generateObject.next());
 // console.log(generateObject.next());
 
-function* generateId() {
-  let id = 1;
-  while (true) {
-    const increment = yield id;
-    if (increment != null) {
-      id += increment;
-    } else {
-      id++;
-    }
-  }
+// function* generateId() {
+//   let id = 1;
+//   while (true) {
+//     const increment = yield id;
+//     if (increment != null) {
+//       id += increment;
+//     } else {
+//       id++;
+//     }
+//   }
+// }
+
+// const generateObject = generateId();
+// // console.log(generateObject.next());
+// // console.log(generateObject.next());
+// // console.log(generateObject.return(8));
+// // console.log(generateObject.next());
+
+// console.log(generateObject.next());
+// console.log(generateObject.next());
+// console.log(generateObject.throw(new Error("Hi")));
+// console.log(generateObject.next());
+
+// function* generateSequence() {
+//   yield 1;
+//   yield 2;
+//   return 3;
+// }
+
+// let generator = generateSequence();
+// let one = generator.next();
+// let two = generator.next();
+// let three = generator.next();
+
+// console.log(JSON.stringify(one));
+// console.log(JSON.stringify(two));
+// console.log(JSON.stringify(three));
+
+// function* generateSequence() {
+//   yield 1;
+//   yield 2;
+//   return 3;
+// }
+
+// let generator = generateSequence();
+
+// for (let value of generator) {
+//   alert(value); // 1, then 2
+// }
+
+// function* generateSequence() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+// let generator = generateSequence();
+
+// for (let value of generator) {
+//   alert(value); // 1, then 2
+// }
+
+// function* generateSequence() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+// let sequence = [0, ...generateSequence()];
+
+// console.log(sequence); // 0, 1, 2, 3
+
+// let range = {
+//   from: 1,
+//   to: 5,
+//   [Symbol.iterator]() {
+//     return {
+//       current: this.from,
+//       last: this.to,
+//       next() {
+//         if (this.current <= this.last) {
+//           return { done: false, value: this.current++ };
+//         } else {
+//           return { done: true };
+//         }
+//       },
+//     };
+//   },
+// };
+
+// alert([...range]);
+
+// let range = {
+//   from: 1,
+//   to: 5,
+
+//   *[Symbol.iterator]() {
+//     // a shorthand for [Symbol.iterator]: function*()
+//     for (let value = this.from; value <= this.to; value++) {
+//       yield value;
+//     }
+//   },
+// };
+
+// alert([...range]); // 1,2,3,4,5
+
+// function* generateSequence(start, end) {
+//   for (let i = start; i <= end; i++) yield i;
+// }
+
+// function* generatePasswordCodes() {
+//   // 0..9
+//   yield* generateSequence(48, 57);
+
+//   // A..Z
+//   yield* generateSequence(65, 90);
+
+//   // a..z
+//   yield* generateSequence(97, 122);
+// }
+
+// let str = "";
+
+// for (let code of generatePasswordCodes()) {
+//   str += String.fromCharCode(code);
+// }
+
+// console.log(str); // 0..9A..Za..z
+
+function* generateSequence(start, end) {
+  for (let i = start; i <= end; i++) yield i;
 }
 
-const generateObject = generateId();
-// console.log(generateObject.next());
-// console.log(generateObject.next());
-// console.log(generateObject.return(8));
-// console.log(generateObject.next());
+function* generateAlphaNum() {
+  // yield* generateSequence(48, 57);
+  for (let i = 48; i <= 57; i++) yield i;
 
-console.log(generateObject.next());
-console.log(generateObject.next());
-console.log(generateObject.throw(new Error("Hi")));
-console.log(generateObject.next());
+  // yield* generateSequence(65, 90);
+  for (let i = 65; i <= 90; i++) yield i;
+
+  // yield* generateSequence(97, 122);
+  for (let i = 97; i <= 122; i++) yield i;
+}
+
+let str = "";
+
+for (let code of generateAlphaNum()) {
+  str += String.fromCharCode(code);
+}
+
+console.log(str); // 0..9A..Za..z
